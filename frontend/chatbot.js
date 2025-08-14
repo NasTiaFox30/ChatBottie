1630d6b98bc0f6718fe1f0a32d7236008c5d5347
Nastia Bzova <bzovanastia@gmail.com>
2025-08-14 18:57:57 +0200

    created js file (with const varibales)

---
+++ frontend/chatbot.js
@@ -0,0 +1,6 @@
+const messagesEl = document.getElementById('messages');
+const chatForm   = document.getElementById('chatForm');
+const userInput  = document.getElementById('userInput');
+const fileInput  = document.getElementById('fileInput');
+const importBtn  = document.getElementById('importCmsBtn');
+const clearBtn   = document.getElementById('clearBtn');
