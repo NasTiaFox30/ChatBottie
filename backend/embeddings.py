480b0e57f76398b0ad3dc9549fdf14f06d0d7716
Nastia Bzova <bzovanastia@gmail.com>
2025-08-18 20:10:49 +0200

    added Cohere API_key

--- backend/embeddings.py
+++ backend/embeddings.py
@@ -5,7 +5,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
+COHERE_API_KEY = os.getenv("COHERE_API_KEY")
+if not COHERE_API_KEY:
+    raise RuntimeError("X - Brak COHERE_API_KEY w .env")
 
 
 def embed_texts(texts: List[str]) -> List[List[float]]:
