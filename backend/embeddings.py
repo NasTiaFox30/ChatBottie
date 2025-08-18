2b6b9f903b2b1599c460c0c9f5f53b7a3946b6f0
Nastia Bzova <bzovanastia@gmail.com>
2025-08-18 20:09:34 +0200

    remove local model

--- backend/embeddings.py
+++ backend/embeddings.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
 
 def embed_texts(texts: List[str]) -> List[List[float]]:
     """
     Akceptuje listę tekstów, zwraca listę wektorów.
     """
-    return _model.encode(texts, convert_to_numpy=True).tolist()
+    return
