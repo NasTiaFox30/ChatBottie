40fdf00276fbadfda5ff3eb00ff47a1f1ada7a7b
Nastia Bzova <bzovanastia@gmail.com>
2025-08-18 20:11:11 +0200

    added _client and _MODEL

--- backend/embeddings.py
+++ backend/embeddings.py
@@ -9,6 +9,9 @@ COHERE_API_KEY = os.getenv("COHERE_API_KEY")
 if not COHERE_API_KEY:
     raise RuntimeError("X - Brak COHERE_API_KEY w .env")
 
+_client = cohere.Client(COHERE_API_KEY)
+
+_MODEL = "embed-multilingual-v3.0"
 
 def embed_texts(texts: List[str]) -> List[List[float]]:
     """
